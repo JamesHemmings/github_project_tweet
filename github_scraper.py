58f3107b2ebdd1aa8ac3f85d26e42100313912e2
james <james.richard.hemmings@gmail.com>
2022-07-08 13:33:36 -0700

    check if repos have been previously posted and if repos are new

--- github_scraper.py
+++ github_scraper.py
@@ -1,3 +1,27 @@
 from github import Github
 import os
+from datetime import datetime
 
+now = datetime.now()
+
+TOKEN = os.getenv("GITHUB_TOKEN")
+
+github = Github(TOKEN)
+
+user = github.get_user()
+
+
+def new_repos():
+    repo_tweets = []
+    with open("previously_posted_repos", "r+") as f:
+        already_posted = f.read()
+        for repo in user.get_repos():
+            if repo.name in already_posted:
+                print(repo.name + " already posted")
+            if repo.created_at.date() == now.date() and repo.name not in already_posted:
+                print(repo.name + " posted")
+                print(repo.description)
+                repo_tweets.append(
+                    f"check out my new repo on github '{repo.name}'!\nDescription:{repo.description}\n {repo.html_url}")
+                f.write(repo.name + "\n")
+    return repo_tweets
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import time
+
 from twitter_poster import TwitterPoster
+from github_scraper import new_repos
+
 if __name__ == '__main__':
     tweeter= TwitterPoster()
 
-    tweeter.tweet("test")
-
+    for repo in new_repos():
+        tweeter.tweet(message=repo)
+        time.sleep(1)
+    tweeter.driver.close()
